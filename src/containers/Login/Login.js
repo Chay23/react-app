9e26a0e227383fafe64ba967fd243b19cf09a12e
BogVin <spongebob69961@gmail.com>
2020-11-27 12:41:44 +0200

    Add styles for Login &  Registration components

--- src/containers/Login/Login.js
+++ src/containers/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import style from './Login.module.css';
 class Login extends Component{
     
     state = {
@@ -9,11 +9,11 @@ class Login extends Component{
 
     render(){
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}></form>
-                Login <input type="text" name="email" value={this.props.login} onChange={this.props.handleLogin} /><br/>
-                Password <input type="password" name="password" value={this.props.password} onChange={this.props.handleLogin} />
-                <input type="submit" value="Sign in" onClick={this.props.handleToken}/>
+            <div className="container form-group text-center">
+                <form onSubmit={this.handleSubmit}  ></form>
+                Login <input  className="form-control" type="text" name="email" value={this.props.login} onChange={this.props.handleLogin } /><br/>
+                Password < input className="form-control"  type="password" name="password" value={this.props.password} onChange={this.props.handleLogin} />
+                <input className={style.button} type="submit" value="Sign in" onClick={this.props.handleToken}/>
             </div>
         );
     }
